1223638935262d214de024639f42b685ed7dd7f3
rsstdd <rosstod@gmail.com>
2016-10-25 16:36:42 -0700

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'ryansobol/browser',
+    'ryansobol/es5',
+    'ryansobol/es6',
+    'ryansobol/jquery',
+    'ryansobol/node',
+    'ryansobol/materialize',
+    'ryansobol/express'
+  ]
+};
