e753fc948ea7a6665df83eca10c5126f0cf7d282
rsstdd <rosstod@gmail.com>
2017-02-13 15:34:43 -0800

    Reformat Migration

--- migrations/20161025171121_airplanes.js
+++ migrations/20161025171121_airplanes.js
@@ -3,16 +3,31 @@
 exports.up = function(knex) {
   return knex.schema.createTable('airplanes', (table) => {
     table.increments();
-    table.string('name').notNullable().defaultTo('');
-    table.text('description').notNullable().defaultTo('');
-    table.integer('year_in_service').notNullable();
-    table.string('country_of_origin').notNullable();
-    table.text('operators').notNullable().defaultTo('');
-    table.integer('max_speed').notNullable();
-    table.integer('max_range').notNullable();
-    table.integer('ceiling').notNullable();
-    table.text('engines').notNullable().defaultTo('');
-    table.text('img_url').notNullable().defaultTo('');
+    table.string('name')
+         .notNullable()
+         .defaultTo('');
+    table.text('description')
+         .notNullable()
+         .defaultTo('');
+    table.integer('year_in_service')
+         .notNullable();
+    table.string('country_of_origin')
+         .notNullable();
+    table.text('operators')
+         .notNullable()
+         .defaultTo('');
+    table.integer('max_speed')
+         .notNullable();
+    table.integer('max_range')
+         .notNullable();
+    table.integer('ceiling')
+         .notNullable();
+    table.text('engines')
+         .notNullable()
+         .defaultTo('');
+    table.text('img_url')
+         .notNullable()
+         .defaultTo('');
     table.timestamps(true, true);
   });
 };
